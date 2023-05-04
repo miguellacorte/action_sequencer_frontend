7ce99f0afa4e8c7f587c313fd9bf811978fe8deb
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2023-05-04 15:58:55 +0200

    styling upgrade

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,91 +1,108 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import SmallParticipationList from "../components/SmallParticipationList";
 import { Link } from "react-router-dom";
+import Draggable from "react-draggable";
+import "../styles/Btns.css";
 import CompositionRecall from "../components/CompositionRecall";
 import "../styles/Home.css";
 
 export default function Home({ usersWithCompositions }) {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [showPopup, setShowPopup] = useState(true);
 
-  useEffect(() => {
-    function handleResize() {
-      setIsMobile(window.innerWidth <= 768);
-    }
-
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
+  console.log(usersWithCompositions);
   if (usersWithCompositions.length !== 0) {
-    const totalUsers = usersWithCompositions.length;
-    const lastUser = usersWithCompositions[totalUsers - 1];
-    const lastUserCompositions = lastUser.compositions;
-    const LastUserCompositionNumber = lastUserCompositions.length;
-    const user = lastUserCompositions[LastUserCompositionNumber - 1];
+    let totalUsers = usersWithCompositions.length;
+    let lastUser = usersWithCompositions[totalUsers - 1];
+    let lastUserCompositions = lastUser.compositions;
+    let LastUserCompositionNumber = lastUserCompositions.length;
+    let user = lastUserCompositions[LastUserCompositionNumber - 1];
 
     return (
       <div>
-        {isMobile && (
-          <div className="mobilePopup">
-            <div className="mobilePopupText">
-              <p>
-                This website has been optimized for desktop. Please visit us on
-                a computer for the best experience.
-              </p>
+        {showPopup && (
+          <div className="popup">
+            <div className="popup-message">
+              This page has been optimized for desktop. Please visit us on your
+              computer for the best experience.
+              <button onClick={() => setShowPopup(false)}>Close</button>
             </div>
           </div>
         )}
 
-        <div className="navbar">
-          <div className="navbarBrand">
-            <h1>Participation Art</h1>
-          </div>
-
-          <div className="navbarLinks">
-            <Link to="/participationHistory" className="navbarLink">
-              Participation history
-            </Link>
-            <Link to="/playground" className="navbarLink">
-              Create your own participation
-            </Link>
-          </div>
-        </div>
-
-        <div className="homeContent">
-          <div className="participationList">
-            <fieldset className="participationListTitle">
+        <Draggable handle="#handle">
+          <div className="boxParticipationList">
+            <fieldset
+              id="handle"
+              style={{
+                fontSize: "13px",
+                position: "relative",
+                left: "-2px",
+                color: "white",
+                backgroundColor: "black",
+                borderColor: "black",
+                borderBlockHeight: "200px",
+                borderStyle: "solid",
+                borderWidth: "1px",
+                padding: "3px",
+                width: "262px",
+              }}
+            >
               Participation history List
             </fieldset>
-
             <SmallParticipationList
               usersWithCompositions={usersWithCompositions}
             />
-
-            <Link to="/participationHistory" className="viewParticipationLink">
-              View complete participation history List
+            <Link
+              to="/participationHistory"
+              style={{
+                fontSize: "10.5px",
+                position: "relative",
+                color: "black",
+                padding: "0px",
+                width: "270px",
+              }}
+            >
+              See complete participation history List ⬈
             </Link>
           </div>
+        </Draggable>
 
-          <div className="compositionRecall">
-            <fieldset className="compositionRecallTitle">
-              Last user participation:
-            </fieldset>
+        <div>
+          <Draggable handle="#handle">
+            <div className="compositionRecallHome">
+              <fieldset
+                id="handle"
+                style={{
+                  fontSize: "13px",
+                  position: "relative",
+                  left: "-2px",
+                  color: "white",
+                  backgroundColor: "black",
+                  borderColor: "black",
+                  borderBlockHeight: "200px",
+                  borderStyle: "solid",
+                  borderWidth: "1px",
+                  padding: "3px",
+                  width: "65.5vw",
+                }}
+              >
+                Last user participation:
+              </fieldset>
 
-            <CompositionRecall
-              drawingX={user.drawingX}
-              drawingY={user.drawingY}
-              notes={user.notes}
-            />
-
-            <Link to="/playground" className="createParticipationLink">
-              Create your own participation
-            </Link>
-          </div>
+              <CompositionRecall
+                drawingX={user.drawingX}
+                drawingY={user.drawingY}
+                notes={user.notes}
+              />
+              <Link to="/playground" className="saveCompositionBtnHome">
+                <button>create your own participation</button>
+              </Link>
+            </div>
+          </Draggable>
         </div>
       </div>
     );
   } else {
     return <div>Loading...</div>;
-  }
+}
 }
