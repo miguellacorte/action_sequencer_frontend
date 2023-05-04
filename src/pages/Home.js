de66a90084f5b70dccdefa2b315ff441603b3bee
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2023-05-04 15:44:11 +0200

    upgrade styling on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,93 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SmallParticipationList from "../components/SmallParticipationList";
 import { Link } from "react-router-dom";
-import Draggable from "react-draggable";
-import "../styles/Btns.css";
 import CompositionRecall from "../components/CompositionRecall";
+import "../styles/Home.css";
 
 export default function Home({ usersWithCompositions }) {
-  console.log(usersWithCompositions);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth <= 768);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   if (usersWithCompositions.length !== 0) {
-    let totalUsers = usersWithCompositions.length;
-    let lastUser = usersWithCompositions[totalUsers - 1];
-    let lastUserCompositions = lastUser.compositions;
-    let LastUserCompositionNumber = lastUserCompositions.length;
-    let user = lastUserCompositions[LastUserCompositionNumber - 1];
+    const totalUsers = usersWithCompositions.length;
+    const lastUser = usersWithCompositions[totalUsers - 1];
+    const lastUserCompositions = lastUser.compositions;
+    const LastUserCompositionNumber = lastUserCompositions.length;
+    const user = lastUserCompositions[LastUserCompositionNumber - 1];
 
     return (
       <div>
-        <Draggable handle="#handle">
-          <div className="boxParticipationList">
-            <fieldset
-              id="handle"
-              style={{
-                fontSize: "13px",
-                position: "relative",
-                left: "-2px",
-                color: "white",
-                backgroundColor: "black",
-                borderColor: "black",
-                borderBlockHeight: "200px",
-                borderStyle: "solid",
-                borderWidth: "1px",
-                padding: "3px",
-                width: "262px",
-              }}
-            >
+        {isMobile && (
+          <div className="mobilePopup">
+            <div className="mobilePopupText">
+              <p>
+                This website has been optimized for desktop. Please visit us on
+                a computer for the best experience.
+              </p>
+            </div>
+          </div>
+        )}
+
+        <div className="navbar">
+          <div className="navbarBrand">
+            <h1>Participation Art</h1>
+          </div>
+
+          <div className="navbarLinks">
+            <Link to="/participationHistory" className="navbarLink">
+              Participation history
+            </Link>
+            <Link to="/playground" className="navbarLink">
+              Create your own participation
+            </Link>
+          </div>
+        </div>
+
+        <div className="homeContent">
+          <div className="participationList">
+            <fieldset className="participationListTitle">
               Participation history List
             </fieldset>
+
             <SmallParticipationList
               usersWithCompositions={usersWithCompositions}
             />
-            <Link
-              to="/participationHistory"
-              style={{
-                fontSize: "10.5px",
-                position: "relative",
-                color: "black",
-                padding: "0px",
-                width: "270px",
-              }}
-            >
-              See complete participation history List ⬈
+
+            <Link to="/participationHistory" className="viewParticipationLink">
+              View complete participation history List
             </Link>
           </div>
-        </Draggable>
 
-        <div>
-          <Draggable handle="#handle">
-            <div className="compositionRecallHome">
-              <fieldset
-                id="handle"
-                style={{
-                  fontSize: "13px",
-                  position: "relative",
-                  left: "-2px",
-                  color: "white",
-                  backgroundColor: "black",
-                  borderColor: "black",
-                  borderBlockHeight: "200px",
-                  borderStyle: "solid",
-                  borderWidth: "1px",
-                  padding: "3px",
-                  width: "65.5vw",
-                }}
-              >
-                Last user participation:
-              </fieldset>
+          <div className="compositionRecall">
+            <fieldset className="compositionRecallTitle">
+              Last user participation:
+            </fieldset>
 
-              <CompositionRecall
-                drawingX={user.drawingX}
-                drawingY={user.drawingY}
-                notes={user.notes}
-              />
-              <Link to="/playground" className="saveCompositionBtnHome">
-                <button>create your own participation</button>
-              </Link>
-            </div>
-          </Draggable>
+            <CompositionRecall
+              drawingX={user.drawingX}
+              drawingY={user.drawingY}
+              notes={user.notes}
+            />
+
+            <Link to="/playground" className="createParticipationLink">
+              Create your own participation
+            </Link>
+          </div>
         </div>
       </div>
     );
+  } else {
+    return <div>Loading...</div>;
   }
 }
