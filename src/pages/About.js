6f8a8170dd495a48053d0a2ac6e6150e8f57ed9a
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2023-05-05 17:25:45 +0200

    added image

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,13 +11,13 @@ export default function About() {
         possibilities of simple interactive music distribution through modern
         internet standards.
       </p>
-      <img src="../" alt="" />
+      <img src="https://github.com/miguellacorte/action_sequencer_frontend/blob/76bf9218aaa08be2ca9146d6f8bda26e9f50f6af/build/cage1958.jpeg" alt="cage" />
       <h3>Technical framework</h3>
       <p className="about-paragraph">
         The project is built using <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> and <a href="https://p5js.org/" target="_blank" rel="noopener noreferrer">p5.js</a> for the frontend and <a href="https://nodejs.org/" target="_blank" rel="noopener noreferrer">Node.js</a> and <a href="https://www.mongodb.com/" target="_blank" rel="noopener noreferrer">MongoDB</a> for the backend. The frontend uses several libraries such as <a href="https://reactrouter.com/" target="_blank" rel="noopener noreferrer">React Router</a>, <a href="https://github.com/react-grid-layout/react-grid-layout" target="_blank" rel="noopener noreferrer">React Draggable</a>, <a href="https://react-bootstrap.github.io/" target="_blank" rel="noopener noreferrer">React Bootstrap</a>, and <a href="https://github.com/CookPete/react-player" target="_blank" rel="noopener noreferrer">React Player</a>, among others. The backend is built using <a href="https://expressjs.com/" target="_blank" rel="noopener noreferrer">Express.js</a> and <a href="https://mongoosejs.com/" target="_blank" rel="noopener noreferrer">Mongoose</a>.
         <br />
         <br />
-        Behind the scenes, p5.js is used to create the grid and handle the user interactions with the interface. <a href="https://tonejs.github.io/" target="_blank" rel="noopener noreferrer">Tone.js</a> is used to load and play the sound samples, as well as to handle the sequencing of the composition. The backend server is responsible for storing and serving the saved compositions.
+        Behind the scenes, p5.js is used to create the grid and handle the user interactions with the interface. <a href="https://tonejs.github.io/" target="_blank" rel="noopener noreferrer">Tone.js</a> is used to synthesize the sound samples, as well as to handle the sequencing of the composition. The backend server is responsible for storing and serving the saved compositions.
       </p>
       <h3>Licensing and references</h3>
       <p className="about-paragraph">
